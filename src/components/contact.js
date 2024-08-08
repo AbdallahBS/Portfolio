03314507407fb2399ed847366ee60165fc7e05e9
Abdallah Ben Salem <abenssalem32@gmail.com>
2024-08-08 18:37:22 +0100

    first commit

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,16 +1,7 @@
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 
-
 function Contact() {
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    // Simulate form submission process
-    // If successful, redirect to thank-you page
-    window.location.href = '/thank-you';
-  };
-
   return (
     <div className="section contact" id="contact" style={{ position: 'relative', overflow: 'hidden' }}>
       <MapContainer
@@ -34,7 +25,12 @@ function Contact() {
           <div className="col-lg-8">
             <div className="contact-form-card">
               <h4 className="contact-title">Send a message</h4>
-              <form name="contact" method="POST" data-netlify="true" onSubmit="submit">
+              <form 
+                name="contact" 
+                method="POST" 
+                data-netlify="true"
+                action="/thank-you"  // Redirect to the thank-you page
+              >
                 <input type="hidden" name="form-name" value="contact" />
                 <div className="form-group">
                   <input className="form-control" type="text" name="name" placeholder="Name *" required />
@@ -52,40 +48,7 @@ function Contact() {
             </div>
           </div>
           <div className="col-lg-4">
-            <div className="contact-info-card">
-              <h4 className="contact-title">Get in touch</h4>
-              <div className="row mb-2">
-                <div className="col-1 pt-1 mr-1">
-                  <i className="ti-mobile icon-md" />
-                </div>
-                <div className="col-10 ">
-                  <h6 className="d-inline">Phone : <br /> <span className="text-muted">+ (216) 20-580-395</span></h6>
-                </div>
-              </div>
-              <div className="row mb-2">
-                <div className="col-1 pt-1 mr-1">
-                  <i className="ti-map-alt icon-md" />
-                </div>
-                <div className="col-10">
-                  <h6 className="d-inline">Address :<br /> <span className="text-muted">El Haouaria, Nabeul: 8045.</span></h6>
-                </div>
-              </div>
-              <div className="row mb-2">
-                <div className="col-1 pt-1 mr-1">
-                  <i className="ti-envelope icon-md" />
-                </div>
-                <div className="col-10">
-                  <h6 className="d-inline">Email :<br /> <span className="text-muted">abdallahbenselam@gmail.com</span></h6>
-                </div>
-              </div>
-              <ul className="social-icons pt-4">
-                <li className="social-item"><a className="social-link text-dark" href="#"><i className="ti-facebook" aria-hidden="true" /></a></li>
-                <li className="social-item"><a className="social-link text-dark" href="#"><i className="ti-twitter" aria-hidden="true" /></a></li>
-                <li className="social-item"><a className="social-link text-dark" href="#"><i className="ti-google" aria-hidden="true" /></a></li>
-                <li className="social-item"><a className="social-link text-dark" href="#"><i className="ti-instagram" aria-hidden="true" /></a></li>
-                <li className="social-item"><a className="social-link text-dark" href="#"><i className="ti-github" aria-hidden="true" /></a></li>
-              </ul>
-            </div>
+            
           </div>
         </div>
       </div>
